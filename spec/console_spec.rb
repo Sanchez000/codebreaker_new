46385664cd7f2019532f734e9ddf62ba1340d02c
Sanchez <artemzub000@gmail.com>
2019-01-21 14:16:04 +0000

    complite coverage 95

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Console do
       described_class.new.show_message(:rules)
     end
   end
+
   describe '#main_menu' do
     it 'start the game from command - start' do
       expect_any_instance_of(Console).to receive(:start).once
@@ -40,20 +41,138 @@ RSpec.describe Console do
       described_class.new.menu_options('some new command')
     end
   end
+
   describe '#start' do
     it 'ask user name and save it to User instance' do
       allow_any_instance_of(Console).to receive(:choose_the_difficulty).and_return('easy')
-      #allow_any_instance_of(Console).to receive(:gets).and_return('Vasya')
-      allow_any_instance_of(Console).to receive(:game_guess)
-      allow_any_instance_of(Console).to receive(:game_guess).and_return('1234')
-      # allow_any_instance_of(Console).to receive(:gets).and_return('exit')
-      #binding.pry
-      #allow_any_instance_of(Console).to receive(:game_guess).with('1234')
-      #allow_any_instance_of(Console).to receive(:gets).and_return('exit')
+      allow_any_instance_of(Console).to receive(:game_guess).and_return('++++')
+      allow_any_instance_of(Console).to receive(:want_to?).and_return(false)
       expect_any_instance_of(Console).to receive(:take_and_save_user)
       expect_any_instance_of(Console).to receive(:choose_the_difficulty)
-      #expect_any_instance_of(Console).to receive(:game_guess)
+      expect_any_instance_of(Console).to receive(:game_process)
       described_class.new.start
+    end
+  end
+
+  describe '#game_guess(guess)' do
+    it 'take guess, validate and return error if guess is not valid' do
+      console = described_class.new
+      expect_any_instance_of(Console).to receive(:show_message).with(:not_allowed)
+      console.game_guess('+++6')
+    end
+    it 'return hint if command get hint' do
+      console = described_class.new
+      expect_any_instance_of(Console).to receive(:game_hint)
+      console.game_guess(Console::GAMEPLAY_COMMANDS[:hint])
+    end
+    it 'return round result if guess are valid' do
+      console = described_class.new
+      user = double('User', name: 'Petya')
+      level = :easy
+      console.game = Game.new(user, level)
+      allow_any_instance_of(Game).to receive(:secret).and_return([4, 2, 3, 1])
+      expect(STDOUT).to receive(:puts).with(I18n.t(:round_result, result: '++--'))
+      console.game_guess('1234')
+    end
+  end
+
+  describe '#game_hint' do
+    it 'return hint if counter hint more then zero' do
+      console = described_class.new
+      user = double('User', name: 'Petya')
+      level = :easy
+      console.game = Game.new(user, level)
+      expect(console).to receive(:show_message)
+      console.game_hint
+    end
+  end
+
+  describe '#want_to?' do
+    it 'return true if user answer equivalent  -> Y' do
+      allow_any_instance_of(Console).to receive(:gets).and_return(Console::GAMEPLAY_COMMANDS[:yes])
+      expect(described_class.new.want_to?(Console::GAMEPLAY_COMMANDS[:restart])).to be true
+    end
+    it 'return false if user answer not equivalent  -> Y' do
+      allow_any_instance_of(Console).to receive(:gets).and_return('n')
+      expect(described_class.new.want_to?(Console::GAMEPLAY_COMMANDS[:save])).to be false
+    end
+  end
+
+  describe '#take_and_save_user' do
+    it 'gets player name and validate it' do
+      console = described_class.new
+      allow_any_instance_of(Console).to receive(:loop).and_yield
+      allow(console).to receive(:gets).and_return('Vasya')
+      #expect(console).to receive(:show_message).with(:give_us_your_name)
+      console.take_and_save_user
+      expect(console.user).to be_instance_of(User)
+    end
+    it 'return error if user name length is not valid' do
+      console = described_class.new
+      allow_any_instance_of(Console).to receive(:loop).and_yield
+      allow(console).to receive(:gets).and_return('++')
+      expect(console).to receive(:show_message).with(:give_us_your_name)
+      expect(console).to receive(:show_message).with(:invalid_name,min: User::NAME_LENGTH.begin, max: User::NAME_LENGTH.end)
+      console.take_and_save_user
+    end
+  end
+
+  describe '#choose_the_difficulty' do
+    it 'return error if user entered not correct difficulty' do
+      console = described_class.new
+      allow_any_instance_of(Console).to receive(:loop).and_yield
+      allow(console).to receive(:gets).and_return('hel')
+      expect(console).to receive(:show_message).with(:choose_difficult, options: Game::LEVELS_NAMES.keys.join(', '))
+      expect(console).to receive(:show_message).with(:unexpected_command)
+      console.choose_the_difficulty
+    end
+    it 'return choosen difficulty if user entered valid answer' do
+      console = described_class.new
+      allow_any_instance_of(Console).to receive(:loop).and_yield
+      allow(console).to receive(:gets).and_return('hell')
+      expect(console).to receive(:show_message).with(:choose_difficult, options: Game::LEVELS_NAMES.keys.join(', '))
+      expect(console.choose_the_difficulty).to eq 'hell'
+    end
+  end
+
+  describe '#game_process' do
+    it 'ask user to enter guess and validate it' do
+      console = described_class.new
+      user = double('User', name: 'Petya')
+      level = :easy
+      console.game = Game.new(user, level)
+      allow_any_instance_of(Game).to receive(:secret).and_return([1, 2, 3, 4])
+      allow_any_instance_of(Console).to receive(:loop).and_yield
+      allow(console).to receive(:gets).and_return('1234')
+      expect(console).to receive(:game_result).with(Game::STATUSES[:win])
+      console.game_process
+    end
+  end
+  
+  describe '#main_menu' do
+    it 'print welcome message' do
+      console = described_class.new
+      allow_any_instance_of(Console).to receive(:loop).and_yield
+      allow(console).to receive(:gets).and_return('')
+      expect(console).to receive(:show_message).with(:welcome)
+      expect(console).to receive(:show_message).with(:menu_options, options: Console::COMMANDS.values.join(' | '))
+      expect(console).to receive(:show_message).with(:unexpected_command)
+      console.main_menu
+    end
+  end
+
+  describe 'game_result' do
+    it 'offers to save result of game if game state win' do
+      console = described_class.new
+      allow(console).to receive(:want_to?).with(Console::GAMEPLAY_COMMANDS[:save]).and_return(true)
+      allow(console).to receive(:want_to?).with(Console::GAMEPLAY_COMMANDS[:restart]).and_return(false)
+      user = double('User', name: 'Petya')
+      level = :easy
+      console.game = Game.new(user, level)
+      expect(console).to receive(:show_message).with(:game_result, stage: Game::STATUSES[:win], code: console.game.secret_code)
+      expect(console).to receive(:show_message).with(:ask_for_save)
+      expect(console).to receive(:show_message).with(:want_restart)
+      console.send(:game_result, Game::STATUSES[:win])
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'simplecov'
 
 SimpleCov.start do
   add_filter '/spec/'
-  # minimum_coverage 95
+  minimum_coverage 95
 end
 
 require 'i18n'
